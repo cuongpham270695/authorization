a47de64490232f715bade6ffe485ac10cdbea0c7
cuongpham270695 <cuong.pham270695@gmail.com>
2021-06-29 14:18:56 +0700

    authorization

---
+++ authorization/database/seeders/UsersTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User();
+        $user->name = "admin";
+        $user->email = "admin@gmail.com";
+        $user->password = bcrypt("admin");
+        $user->admin = 1;
+        $user->save();
+
+        $user = new User();
+        $user->name = "user";
+        $user->email = "user@gmail.com";
+        $user->password = bcrypt("user");
+        $user->admin = 0;
+        $user->save();
+    }
+}
